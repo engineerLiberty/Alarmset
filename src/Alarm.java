fa6582bbcd70491d741780882e38d433d8495c33
engineerLiberty <libertyimobi@gmail.com>
2022-10-02 04:39:36 +0100

    push

--- src/Alarm.java
+++ src/Alarm.java
@@ -6,6 +6,22 @@ public class Alarm {
 
         } else {return false;}
     }
+    public static int trick(int[] x,int n){
+        int result = 0;
+        for (int i=0;i<n; i++){
+           if(x[i]==8) {
+                x[i] += 5;
+            }
+            else if (!(x[i]%2==0)) {
+
+                x[i] += 3;
+            } else if (x[i]%2==0) {
+                x[i] += 1;
+            }
+            result += x[i];
+        }
+        return result;
+    }
 
 
 }
